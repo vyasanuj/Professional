4c7c231cabf3903013a7caf2492d500bcf267160
vyasanuj <vanuj616@gmail.com>
2024-09-12 14:01:09 +0530

    Adding public/Temp/.gitkeep folders and file again

--- src/middlewares/Auth.middleware.js
+++ src/middlewares/Auth.middleware.js
@@ -3,8 +3,9 @@ import { asycnHandler } from "../utils/asyncHandler.js"
 import jwt from "jsonwebtoken"
 import { User } from "../models/User.model.js"
 
+
+const jwtverify = asycnHandler (async (req , res , next) => {
 try {
-    const jwtverify = asycnHandler (async (req , res , next) => {
         const token = req.cookies?.accessToken || req.header("Authorization")?.replace("bearer " , "")
         if (!token) {
             throw new ApiError (401,"unauthorized request")
@@ -12,7 +13,7 @@ try {
     
         const decodedtoken = jwt.verify(token , process.env.ACCESS_TOKEN_SECRET)
     
-        const user = User.findById(decodedtoken?._id).select("-password - refreshToken")
+        const user = await User.findById(decodedtoken?._id).select("-password - refreshToken")
         if (!user){
             throw new ApiError (402 , "invalide Access Token")
         }
@@ -20,9 +21,13 @@ try {
         // yeeh smaj nahi aaya 
         req.user = user ;
         next()
-    })
-} catch (error) {
-    throw new ApiError(401 , error?.message || "invalide Access Token")
+    }
+
+    catch (error) {
+        throw new ApiError(401 , error?.message || "invalide Access Token")
+    }
 }
+)
 
 export {jwtverify}
+
